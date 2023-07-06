f69d581b5f6d14b1d9f0d85dbd488f6d7ebce01b
Tomiwa Ademidun <tomiademidun@gmail.com>
2023-07-06 12:18:24 -0400

    add script to deploy an NFT

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -66,7 +66,6 @@ task("deploy:market", "Deploys the Market.sol contract")
 
   const account = getAccount(chainId);
   const provider = getProvider(chainId);
-  console.log({provider});
   let gasPrice;
   
   try {
@@ -100,6 +99,59 @@ task("deploy:market", "Deploys the Market.sol contract")
   console.log("\x1b[32m%s\x1b[0m", `View in block explorer: ${chainConfig.BLOCK_EXPLORER_URL}/address/${chainConfig.NFT_MARKETPLACE_ADDRESS}`);
 
 });
+
+task("deploy:rentableNFT", "Deploys the RentableNFT contract")
+.addParam("chainId", "The Chain ID of the blockchain where this contract will be deployed.")
+.setAction(async function (taskArguments, hre) {
+
+  const availableChains = getAvailableChains();
+  const { chainId } = taskArguments;
+
+  if (!(chainId in availableChains)) {
+    let chainsForPrinting = {};
+    Object.values(availableChains).forEach(chain=>{chainsForPrinting[chain.CHAIN_ID]= `${chain.CHAIN_NAME} (${chain.NETWORK_NAME})`});
+    chainsForPrinting = JSON.stringify(chainsForPrinting, null, 4);
+
+    console.error("\x1b[31m%s\x1b[0m", `Invalid chainId: ${chainId}.\nPlease select one of the following:\n${chainsForPrinting}`);
+    process.exit(1);
+  }
+
+  const account = getAccount(chainId);
+  const provider = getProvider(chainId);
+  let gasPrice;
+  
+  try {
+    gasPrice = await provider.getGasPrice();
+  } catch (e) {
+    console.error("\x1b[31m%s\x1b[0m", `Could not get gas price. Please check your network connection. ${e}}`);
+    gasPrice = null;
+  }
+  
+  let RentableNFT;
+  if (account) {
+    RentableNFT = await hre.ethers.getContractFactory("RentableNFT", account);
+  } else {
+    RentableNFT = await hre.ethers.getContractFactory("RentableNFT");
+  }
+  let rentableNFT;
+
+  if (gasPrice) {
+    rentableNFT = await RentableNFT.deploy({gasPrice});
+  } else {
+    rentableNFT = await RentableNFT.deploy();
+  }
+  await rentableNFT.deployed();
+
+
+  availableChains[chainId].RENTABLE_NFT_ADDRESS = rentableNFT.address.toLowerCase();
+  fs.writeFileSync(chainConfigFilePath, JSON.stringify(availableChains, null, 4));
+
+  const chainConfig = availableChains[chainId];
+  console.log("\x1b[32m%s\x1b[0m", `RentableNFT deployed to ${chainConfig.CHAIN_NAME} (${chainConfig.NETWORK_NAME}): ${chainConfig.RENTABLE_NFT_ADDRESS}`);
+  console.log("\x1b[32m%s\x1b[0m", `View in block explorer: ${chainConfig.BLOCK_EXPLORER_URL}/address/${chainConfig.RENTABLE_NFT_ADDRESS}`);
+
+});
+
 
 task("deploy", "Deploys the Market.sol and NFT.sol contract")
 .addParam("chainId", "The Chain ID of the blockchain where this contract will be deployed.")
